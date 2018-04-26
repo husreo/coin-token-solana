c2b4850b99aa788852c26b21f1aaae7ff11b2f45
husreo <setodev1994@gmail.com>
2018-04-26 20:13:40 +0300

    add first test

---
+++ 3_test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+echo "Cleaning..."
+rm -rf \
+    Tests/bin \
+    Tests/obj
+
+echo "Publishing tests..."
+dotnet publish Tests --configuration Release
+
+echo "Executing tests..."
+dotnet test Tests --no-build --configuration Release
