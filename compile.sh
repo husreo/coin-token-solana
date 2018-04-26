289b663005f090b89718a6c0d2e239756657a490
husreo <setodev1994@gmail.com>
2018-04-26 14:18:07 +0300

    using 'dotnet publish' instead 'dotnet build'

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 neon_path=~/D_DRIVE/ProgramFiles/neo-compiler/neon/bin/Debug/netcoreapp2.0/publish/neon.dll
-output_path=$(pwd)/bin/Debug/netstandard2.0
+output_path=$(pwd)/bin/Debug/netstandard2.0/publish
 
 set -e
 
@@ -8,11 +8,7 @@ echo "Make clean..."
 source clean.sh
 
 echo "Building project..."
-dotnet build
-
-echo "Copying Net.Framework to output directory..."
-cp ~/.nuget/packages/neo.smartcontract.framework/2.7.3/lib/netstandard1.6/Neo.SmartContract.Framework.dll \
-$output_path/Neo.SmartContract.Framework.dll
+dotnet publish
 
 echo "Compiling dll to avm..."
 dotnet $neon_path $output_path/NEP5Token.dll
